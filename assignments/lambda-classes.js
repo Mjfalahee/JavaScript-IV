8cfe1844d9e91ab17013a00f49cca2dd19c987f9
mjfalahee <mjfalahee@gmail.com>
2019-04-04 14:12:20 -0700

    Created Project Manager class, and a couple test objects to use for debugging. I also finished up the student class including its methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -20,7 +20,7 @@ class Person {
 
 
 // Instructor class
-// props: specialty, favLanguage, catchPhrase
+// props: specialty, favLanguage, catchPhrase // props from parent: name, age, location, gender
 // method: demo, grade
 
 class Instructor extends Person {
@@ -41,7 +41,7 @@ class Instructor extends Person {
 }
 
 //Student Class
-//props: previousBackground, className, favSubjects
+//props: previousBackground, className, favSubjects // props from parent: name, age, location, gender
 //methods: listsSubjects, PRAssignment, sprintChallenge
 
 class Student extends Person {
@@ -52,10 +52,13 @@ class Student extends Person {
         this.favSubjects = studentinfo.favSubjects;
     }
 
+    // takes array of strings and spits out first string  for each iteration of loop until all subjects have been listed.
     listsSubjects() {
-
+        for (let i=0; i=this.favSubjects.length; i++) {
+            console.log(this.favSubjects.shift());
+        }
     }
-    
+
     PRAssignment (subject) {
         return console.log(`${this.name} has submitted a PR for ${subject}`);
     }
@@ -64,3 +67,46 @@ class Student extends Person {
         return console.log(`${this.name} has begun the sprint challenge on ${subject}`);
     }
 }
+
+// Project Manager class
+// props: gradClassName, favInstructor // props from parents: specialty, favLanguage, catchPhrase, name, age, location, gender
+// methods: standUp, debugsCode
+class ProjectManager extends Instructor {
+    constructor(PMinfo) {
+        super(PMinfo);
+        this.gradClassName = PMinfo.gradClassName;
+        this.favInstructor = PMinfo.favInstructor;
+    }
+
+    standUp(channel) {
+        return console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+
+    debugsCode(student, subject) {
+        return console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`);
+    }
+}
+
+// Test objects for classes
+
+const igeorge = new Instructor({
+    name: 'George',
+    location: 'Paris',
+    age: 31,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `I like to move it.`
+  });
+
+const sphil = new Student({
+    name: 'Phil',
+    location: 'London',
+    age: 25,
+    gender: 'male',
+    previousBackground: 'Barrista',
+    className: 'Web19',
+    favSubjects: ['JavaScript', 'Video Editing', 'Greek Philosophy', 'Cooking']
+}) 
+
+sphil.listsSubjects();
