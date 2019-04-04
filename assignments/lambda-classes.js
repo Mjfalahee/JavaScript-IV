3a07eb55ce69b760ef23b7d11b4e9fa2293a892a
mjfalahee <mjfalahee@gmail.com>
2019-04-04 13:56:36 -0700

    Finished instructor class, and working on student class methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -4,6 +4,7 @@
 // base class of Person
 // props: name, age, location, gender
 // method: speak
+
 class Person {
     constructor(info) {
         this.name = info.name;
@@ -16,6 +17,11 @@ class Person {
         return console.log(`Hello, my name is ${this.name}, and I am from ${this.location}.`);
     }
 }
+
+
+// Instructor class
+// props: specialty, favLanguage, catchPhrase
+// method: demo, grade
 
 class Instructor extends Person {
     constructor(instructorinfo) {
@@ -30,6 +36,31 @@ class Instructor extends Person {
     }
 
     grade(student, subject) {
-        
+        return console.log(`${student.name} receives a perfect score on ${subject}.`);
+    }
+}
+
+//Student Class
+//props: previousBackground, className, favSubjects
+//methods: listsSubjects, PRAssignment, sprintChallenge
+
+class Student extends Person {
+    constructor(studentinfo) {
+        super(studentinfo);
+        this.previousBackground = studentinfo.previousBackground;
+        this.className = studentinfo.className;
+        this.favSubjects = studentinfo.favSubjects;
+    }
+
+    listsSubjects() {
+
+    }
+    
+    PRAssignment (subject) {
+        return console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+
+    sprintChallenge (subject) {
+        return console.log(`${this.name} has begun the sprint challenge on ${subject}`);
     }
 }
